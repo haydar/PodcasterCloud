33dd84f20b4710bafeecbad1e94033c381369757
Haydar ŞAHIN <haydarsahin34@gmail.com>
2018-10-27 01:55:33 +0300

    Episode create and Index page added.

---
+++ resources/views/dashboard/pages/episodeCreate.blade.php
@@ -0,0 +1,10 @@
+@extends('dashboard.layouts.master')
+@section('title',$podcast->name)
+@section('content')
+<div class="content">
+    <div class="row">
+        <div class="col-md-12">
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/dashboard/pages/episodeIndex.blade.php
+++ resources/views/dashboard/pages/episodeIndex.blade.php
@@ -2,9 +2,26 @@
 @section('title',$podcast->name)
 @section('content')
 <div class="content">
-    <div class="card">
-        <div class="card-header">
-            <h3>All Episodes will be here</h3>
+    <div class="row">
+        <div class="col-md-12">
+            @isset($episodes)
+            <div class="card">
+                    <div class="card-header">
+                        <h3>{{$podcast->name}}'s Episodes</h3>
+                    </div>
+                    <div class="card-body">
+                        <table class="table">
+                            <thead class="text-primary">
+                                <tr>
+                                    <th>Episode Title</th>
+                                    <th>Publish Date</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+            </div>
+            @endisset
         </div>
     </div>
 </div>
