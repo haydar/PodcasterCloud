ce7790fa269d96f2f1baef05eff4823be9395c46
Haydar ŞAHIN <haydarsahin34@gmail.com>
2018-10-27 20:55:19 +0300

    DigitalOcean Spaces Env section added

--- config/filesystems.php
+++ config/filesystems.php
@@ -64,6 +64,14 @@ return [
             'url' => env('AWS_URL'),
         ],
 
+        'doSpaces' => [
+            'driver' => 's3',
+            'key' => env('DO_SPACES_KEY'),
+            'secret' => env('DO_SPACES_SECRET'),
+            'region' => env('DO_SPACES_REGION'),
+            'bucket' => env('DO_SPACES_BUCKET'),
+            'endpoint' => env('DO_SPACES_ENDPOINT'),
+        ],
     ],
 
 ];
