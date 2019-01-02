bb641f798c1e3172d4e30ea94a387c438e4ef2fa
Haydar ŞAHIN <haydarsahin34@gmail.com>
2019-01-02 22:04:17 +0300

    error alert changed with sweetalert

--- resources/views/dashboard/pages/episodeCreate.blade.php
+++ resources/views/dashboard/pages/episodeCreate.blade.php
@@ -158,7 +158,11 @@
                             });
                         });
 
-                        alert($errors);
+                        swal({
+                                type: 'error',
+                                title: 'Oops...',
+                                html:$errors
+                        });
                     }
                 });
             } else {
