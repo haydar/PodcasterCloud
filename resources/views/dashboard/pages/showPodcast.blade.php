6274e8bdeb3e2486fe6eb39042dbdecd6de800c4
Haydar ŞAHIN <haydarsahin34@gmail.com>
2019-01-05 03:09:40 +0300

    no episode error added

--- resources/views/dashboard/pages/showPodcast.blade.php
+++ resources/views/dashboard/pages/showPodcast.blade.php
@@ -2,9 +2,27 @@
 @section('title',$podcast->name)
 @section('content')
 <div class="content">
-    <div class="card">
-        <div class="card-header">
-            <h3>{{$podcast->name}}'s statics will be here soon</h3>
+    @empty($podcast->episode)
+    <div class="row">
+        <div class="card col-md-12">
+            <div class="card-header">
+                <h6 class="text-capitalize mb-0">No one at the home yet  <i class="fa fa-home"></i></h6>
+            </div>
+            <div class="card-body">
+                <p class="mb-0">You started your broadcasting life with a clean page. You can upload new one episode or import existing podcast series.</p>
+                <a class="btn btn-success btn-sm text-white" href="https://podcastercloud.dev/manage/podcast/dfvfdvd/episode/create" style="text-transform:none;">
+                    <i class="nc-icon nc-cloud-upload-94"></i> <span class="d-none d-md-inline-block">Create Episode</span>
+                </a>
+                <a class="btn btn-sm btn-info text-capitalize text-white"><i class="fa fa-rss"></i> Import RSS</a>
+            </div>
+        </div>
+    </div>
+    @endempty
+    <div class="row">
+        <div class="card col-md-12">
+            <div class="card-header">
+                <h3>{{$podcast->name}}'s statics will be here soon</h3>
+            </div>
         </div>
     </div>
 </div>
