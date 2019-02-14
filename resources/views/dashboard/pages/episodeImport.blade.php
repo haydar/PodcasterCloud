45e13493843145315652a6129fdd36c8284496ec
Haydar ŞAHIN <haydarsahin34@gmail.com>
2019-02-15 02:07:59 +0300

    queue added

--- resources/views/dashboard/pages/episodeImport.blade.php
+++ resources/views/dashboard/pages/episodeImport.blade.php
@@ -14,10 +14,10 @@
     <div class="card bg-light h-50" id="importEpisodeCard">
         <div class="card-body border-dashed d-flex flex-column align-items-center text-center justify-content-center" id="dropSection">
             <span>You have already a podcast series and you wanna migrate to Podcaster Cloud Services? Don't worry, We thought about this too.</span>
-            <form  class="importEpisodeCard col-md-8" id="importEpisodeCard" enctype="multipart/form-data">
+            <form class="importEpisodeCard col-md-8" id="importEpisodeForm" enctype="multipart/form-data">
                 <label for="importUrl">Feed URL :</label>
-                <input type="url" class="form-control" required name="importUrl" id="importUrl" placeholder="Paste your RSS Feed">
-                <button type="button" class="btn btn-info btn-lg text-white" id="importButton" style="text-transform:none;">
+                <input type="url" class="form-control" name="importUrl" id="importUrl" placeholder="Paste your RSS Feed" required>
+                <button type="submit" class="btn btn-info btn-lg text-white" id="importButton" style="text-transform:none;">
                 <i class="nc-icon nc-cloud-upload-94"></i>  Import Episodes </button>
             </form>
             <small id="wrongFileTypeError" style="display:none;" class="form-text text-danger">You have selected a file with the wrong file type. We can accept only audio files.</small>
@@ -27,10 +27,70 @@
 @endsection
 
 @section('js')
-<script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.28.5/sweetalert2.all.js" integrity="sha256-+yrurPEYDIh9PES+m128Vc0a49Csb6lx0lSzXjX62HQ=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.28.5/sweetalert2.all.js" integrity="sha256-+yrurPEYDIh9PES+m128Vc0a49Csb6lx0lSzXjX62HQ="
+    crossorigin="anonymous"></script>
 <script>
     $(document).ready(function(){
+        var importButton=document.getElementById('importButton');
+        var isFormValid = document.getElementById('importEpisodeForm').checkValidity();
+        var formData= new FormData(document.getElementById('importEpisodeForm'));
 
+        importButton.onclick = function (e) {
+            var importButton=document.getElementById('importButton');
+            var isFormValid = document.getElementById('importEpisodeForm').checkValidity();
+            var formData= new FormData(document.getElementById('importEpisodeForm'));
+
+            if (isFormValid)
+            {
+                e.preventDefault();
+                $.ajax({
+                url:"{{route('podcast.importepisode',[$podcast->slug])}}",
+                dataType:'JSON',
+                type:'POST',
+                enctype: 'text/plain',
+                contentType: false,
+                processData: false,
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                },
+                data:formData,
+                success:function(result){
+                    swal({
+                            type: 'success',
+                            title: "Episodes importing started! We will notify you when it's done",
+                    });
+                },
+                error:function(result){
+                    var $data=jQuery.parseJSON(result.responseText);
+                    $errors="";
+
+                    if (result.status=='422')
+                    {
+                        Object.keys($data.errors).forEach(key => {
+                            $data.errors[key].forEach(errorMessage => {
+                                $errors+=errorMessage+'<br>';
+                            });
+                        });
+
+                        swal({
+                            type: 'error',
+                            title: 'Oops...',
+                            html:$errors
+                        });
+                    }
+                    else
+                    {
+                        swal({
+                            type: 'error',
+                            title: 'Oops...',
+                            text: $data.message,
+                        });
+                    }
+                    },
+                });
+            }
+        }
     });
+
 </script>
 @endsection
