87ab7887d1ca256a8abeac4b008d24dbef857d1d
Haydar ŞAHIN <haydarsahin34@gmail.com>
2019-02-21 04:13:57 +0300

    queue retry after time increased

--- config/queue.php
+++ config/queue.php
@@ -38,7 +38,7 @@ return [
             'driver' => 'database',
             'table' => 'jobs',
             'queue' => 'default',
-            'retry_after' => 180,
+            'retry_after' => 1800,
         ],
 
         'beanstalkd' => [
